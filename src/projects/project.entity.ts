d275591e15f3c22bfb2e6c1638766a8fa5199476
hashir <hashirsmartyy@gmail.com>
2025-03-29 22:53:55 +0500

    setup entity classes for project and server

--- src/projects/project.entity.ts
+++ src/projects/project.entity.ts
@@ -1,11 +1,38 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import { Server } from '../servers/server.entity';
 
 @Entity()
 export class Project {
-  @PrimaryGeneratedColumn()
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   name: string;
 
+  @Column()
   description: string;
+
+  @ManyToOne(() => Server, (server) => server.projects)
+  @JoinColumn({ name: 'serverId' })
+  server: Server;
+
+  @Column()
+  serverId: string;
+
+  @Column()
+  createdAt: Date;
+
+  @Column()
+  updatedAt: Date;
+
+  constructor() {
+    this.createdAt = new Date();
+    this.updatedAt = new Date();
+  }
 }
--- src/servers/server.entity.ts
+++ src/servers/server.entity.ts
@@ -1,1 +1,35 @@
-export class Server {}
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Project } from '../projects/project.entity';
+
+@Entity()
+export class Server {
+  constructor() {
+    this.createdAt = new Date();
+    this.updatedAt = new Date();
+  }
+
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  ip_address: string;
+
+  @Column()
+  status: string;
+
+  //   projects for this server
+  @OneToMany(() => Project, (project) => project.serverId)
+  projects: Project[];
+
+  @Column()
+  createdAt: Date;
+
+  @Column()
+  updatedAt: Date;
+}
