71630008ed49ed3e63786889ef9c93c82b3209e3
hashir <hashirsmartyy@gmail.com>
2025-06-13 14:57:32 +0500

    Added common resource docker compose templates and their configs

--- src/utils/dummy_data.ts
+++ src/utils/dummy_data.ts
@@ -6,8 +6,43 @@ export const dummyConfigData: Array<{
   {
     serviceId: 1,
     pathToConfig:
-      '/home/hashir/hashhost-backend-nest/src/templates/docker-compose/mongo-template.yaml',
+      '/home/hashir/hashhost-backend-nest/src/templates/docker-compose/mongo.hbs',
     pathToOutput:
-      '/home/hashir/hashhost-backend-nest/src/templates/docker-compose/docker-compose-CREATED.yml',
+      '/home/hashir/hashhost-backend-nest/src/templates/processed-docker-compose/docker-compose-MONGO.yml',
+  },
+  {
+    serviceId: 2,
+    pathToConfig:
+      '/home/hashir/hashhost-backend-nest/src/templates/docker-compose/redis.hbs',
+    pathToOutput:
+      '/home/hashir/hashhost-backend-nest/src/templates/processed-docker-compose/docker-compose-REDIS.yml',
+  },
+  {
+    serviceId: 3,
+    pathToConfig:
+      '/home/hashir/hashhost-backend-nest/src/templates/docker-compose/grafana.hbs',
+    pathToOutput:
+      '/home/hashir/hashhost-backend-nest/src/templates/processed-docker-compose/docker-compose-GRAFANA.yml',
+  },
+  {
+    serviceId: 4,
+    pathToConfig:
+      '/home/hashir/hashhost-backend-nest/src/templates/docker-compose/postgres.hbs',
+    pathToOutput:
+      '/home/hashir/hashhost-backend-nest/src/templates/processed-docker-compose/docker-compose-POSTGRES.yml',
+  },
+  {
+    serviceId: 5,
+    pathToConfig:
+      '/home/hashir/hashhost-backend-nest/src/templates/docker-compose/prometheus.hbs',
+    pathToOutput:
+      '/home/hashir/hashhost-backend-nest/src/templates/processed-docker-compose/docker-compose-PROMETHEUS.yml',
+  },
+  {
+    serviceId: 6,
+    pathToConfig:
+      '/home/hashir/hashhost-backend-nest/src/templates/docker-compose/nginx.hbs',
+    pathToOutput:
+      '/home/hashir/hashhost-backend-nest/src/templates/processed-docker-compose/docker-compose-NGINX.yml',
   },
 ];
--- src/utils/resource-configs.ts
+++ src/utils/resource-configs.ts
@@ -13,3 +13,69 @@ export type mongoConfig = {
   networkName: string;
   isNetworkExternal: boolean;
 };
+
+export type redisConfig = {
+  serviceName: string;
+  redisVersion: string;
+  containerName: string;
+  restartPolicy: string;
+  hostPort: string;
+  dataVolume: string;
+  password?: string; // optional, since Redis can run without auth
+  networkName: string;
+  isNetworkExternal: boolean;
+};
+
+export type grafanaConfig = {
+  serviceName: string;
+  grafanaVersion: string;
+  containerName: string;
+  restartPolicy: string;
+  hostPort: string;
+  dataVolume: string;
+  adminUser: string;
+  adminPassword: string;
+  timeZone: string;
+  networkName: string;
+  isNetworkExternal: boolean;
+};
+
+export type postgresConfig = {
+  serviceName: string;
+  postgresVersion: string;
+  containerName: string;
+  restartPolicy: string;
+  hostPort: string;
+  dataVolume: string;
+  dbUser: string;
+  dbPassword: string;
+  dbName: string;
+  timeZone: string;
+  networkName: string;
+  isNetworkExternal: boolean;
+};
+
+export type prometheusConfig = {
+  serviceName: string;
+  prometheusVersion: string;
+  containerName: string;
+  restartPolicy: string;
+  hostPort: string;
+  configVolume: string; // mount path for prometheus.yml
+  dataVolume: string;
+  networkName: string;
+  isNetworkExternal: boolean;
+};
+
+export type nginxConfig = {
+  serviceName: string;
+  nginxVersion: string;
+  containerName: string;
+  restartPolicy: string;
+  httpPort: string;
+  httpsPort: string;
+  configVolume: string; // e.g. ./nginx.conf:/etc/nginx/nginx.conf
+  certsVolume?: string; // if using SSL
+  networkName: string;
+  isNetworkExternal: boolean;
+};
